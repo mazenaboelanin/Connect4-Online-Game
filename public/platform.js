52de6e12828bf0a94be8f36d29e43911f14b955b
Mazen Aboelanin <mazenaboelanin@gmail.com>
2020-03-20 23:51:27 +0200

    last update

---
+++ public/platform.js
@@ -0,0 +1,45 @@
+class Platform {
+    //constructor
+    constructor(
+        rows = 0,
+        cols = 0,
+        x = 0,
+        y = 0,
+        dimension = 25,
+        color = {r:255, g:255, b:255},
+        margin = 2
+    ){
+        this.platform = this.generatePlatform(rows, cols);
+        this.dimension = dimension;
+        this.color = color;
+        this.x = x; 
+        this.y = y;
+        this.margin = margin ; 
+    }
+
+    show(){
+        this.platform.forEach((row, i) =>
+            row.forEach((piece, j) =>
+                piece === null ? this.showEmptySpace(i,j) : piece.show()
+            )
+        );
+    }
+
+    // matrix
+    generatePlatform(rows, cols){
+        return Array.from(new Array(rows), row =>
+            Array.from(new Array(cols), col => null)
+        );
+    }
+
+    showEmptySpace(row, col){
+        let { r, g, b } = this.color;
+        fill(255);
+        stroke(r, g, b);
+        circle(// working like margin 
+            row * this.dimension * this.margin + this.dimension, 
+            col * this.dimension * this.margin + this.dimension,
+            this.dimension
+        );
+    }
+}
--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,54 @@
+const diameter = 50; 
+const xwidth = 600; 
+const yheight = 600 ;
+const rows = 7; 
+const cols = 7 ; 
+const platformMargin = 2;
+
+// 300px in x and in y  , 25 in diameter
+let piece = new Piece(xwidth / 2, yheight / 2, diameter, {r:250, g:10, b:0});
+
+let platform = new Platform (rows, cols, 0, 0, 50, {r:0, g:0,b:0}, platformMargin);
+
 function setup(){
-    createCanvas(600,600);
+    let canv = createCanvas(xwidth,yheight);
+    //canv.postion(300 , 100);
 }
 
 function draw(){
-    background(0);
+    //background color 
+    background(0,0,255);
+    platform.show();
+    piece.show();
+    movementOfPiece(piece);
 }
+
+//movement
+let movementOfPiece = p => {
+    p.x = mouseX;
+    p.y = mouseY;
+}
+
+let putPieceOnPlatform = (piece, platform) => {
+    //map function from p5.js
+    //map(value, start1, stop1, start2, stop2, [withinBounds])
+    let mapX = map(piece.x, 0, width, 0, rows )
+    let mapY = map(piece.y, 0, width, 0, cols )
+
+    // console.log(mapX, mapY);
+    // console.log(floor(mapX), floor(mapY));
+    platform.platform[floor(mapX)][floor(mapY)] = new Piece(piece.x, piece.y, piece.diameter, piece.color);
+    //platform.insert(piece)
+}
+
+//p5.js click function
+function mouseClicked() {
+
+    putPieceOnPlatform(piece, platform);
+
+    // if (value === 0) {
+    //   value = 255;
+    // } else {
+    //   value = 0;
+    // }
+  }
