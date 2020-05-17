7eafb7f454beefb7cf9466d4027bd981f19e8b92
mazenaboelanin <mazenaboelanin@gmail.com>
2020-05-18 00:38:13 +0200

    New Design with SocketIO

---
+++ client/platform.js
@@ -0,0 +1,54 @@
+class Platform {
+    //constructor
+    constructor(
+        rows = 0,
+        cols = 0,
+        x = 0,
+        y = 0,
+        dimension = 25,
+        color = {r:255, g:255, b:255},
+        margin = 2
+    ){
+        this.platform = this.generatePlatform(rows, cols);
+        this.dimension = dimension;
+        this.color = color;
+        this.x = x; 
+        this.y = y;
+        this.margin = margin ; 
+    }
+
+    show(){
+        this.platform.forEach((row, i) =>
+            row.forEach((piece, j) =>
+                piece === null ? this.showEmptySpace(i,j) : piece.show()
+            )
+        );
+    }
+
+    // matrix
+    generatePlatform(rows, cols){
+        return Array.from(new Array(rows), row =>
+            Array.from(new Array(cols), col => null)
+        );
+    }
+
+    existsNeighborAtBottom(row, col){
+        if(col >= this.platform.length){
+            return false;
+        }
+        else{
+            return ! (this.platform[row][col + 1] === null) ;
+        }
+    }
+
+    showEmptySpace(row, col){
+        let { r, g, b } = this.color;
+        fill(255);
+        stroke(r, g, b);
+        circle(// working like margin 
+            row * this.dimension * this.margin + this.dimension, 
+            col * this.dimension * this.margin + this.dimension,
+            this.dimension
+        );
+    }
+}
